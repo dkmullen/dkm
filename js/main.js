da004502e94a46ab3094a9dde140b75aae6c82a1
Dennis Mullen <dkmullen@gmail.com>
2021-02-15 16:45:38 -0500

    Menu template

--- js/main.js
+++ js/main.js
@@ -6,3 +6,20 @@ toggleMenu = () => {
         classList.add('show');
     }
 }
+
+const navTemplate = 
+    `<div class="navbar">
+        <div class="logo">dkmullen.com</div>
+        <div class="menu" id="menu">
+            <div class="menu-item">About</div>
+            <div class="menu-item">Writing</div>
+            <div class="menu-item">Favorite Things</div>
+        </div>
+        <div class="menu-icon-wrapper">
+            <img class = "menu-icon" src="assets/images/menu-white-36dp.svg" onclick="toggleMenu()"> 
+        </div>
+    </div>`;
+
+loadNav = () => {
+    document.getElementById('navbar-wrapper').innerHTML = navTemplate;
+}
