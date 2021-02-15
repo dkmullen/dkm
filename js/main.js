90fe1546c9f4cd1892a8eefaa95a7482227b1097
Dennis Mullen <dkmullen@gmail.com>
2021-02-15 17:06:56 -0500

    More pages

--- js/main.js
+++ js/main.js
@@ -9,11 +9,11 @@ toggleMenu = () => {
 
 const navTemplate = 
     `<div class="navbar">
-        <div class="logo">dkmullen.com</div>
+        <div class="logo"><a href="./index.html">dkmullen.com</a></div>
         <div class="menu" id="menu">
-            <div class="menu-item">About</div>
-            <div class="menu-item">Writing</div>
-            <div class="menu-item">Favorite Things</div>
+            <a href="./about.html"><div class="menu-item">About</div></a>
+            <a href="./writing.html"><div class="menu-item">Writing</div></a>
+            <a href="./favorite-things.html"><div class="menu-item">Favorite Things</div></a>
         </div>
         <div class="menu-icon-wrapper">
             <img class = "menu-icon" src="assets/images/menu-white-36dp.svg" onclick="toggleMenu()"> 
