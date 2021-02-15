f85bb1c3c9a55b76fa4711b41a1cb64c031d00af
Dennis Mullen <dkmullen@gmail.com>
2021-02-15 16:27:39 -0500

    Mobile menu

---
+++ js/main.js
@@ -0,0 +1,8 @@
+toggleMenu = () => {
+    let classList = document.getElementById('menu').classList;
+    if (classList.contains('show')) {
+        classList.remove('show');
+    } else {
+        classList.add('show');
+    }
+}
